1a8d2e853492da547465b8bbf883a58995e0777c
ida <discoverywant30@gmail.com>
2024-01-14 13:02:06 +0900

    各ボタンの動作後にdelayをするようにした. ボタンとマトリクスを両立させた

--- button.ino
+++ button.ino
@@ -1,6 +1,7 @@
 bool buttonFlag = false;
 void buttonA () {
   if (buttonIsON(BUTTON_A)) {
+    Serial.println("a");
     switch (getMode()) {
       case 1://上
       break;
@@ -11,31 +12,34 @@ void buttonA () {
       case 4://右
       break;
     }
+    delay(100);
   }
 }
 
 
 void buttonB () {
   if (buttonIsON(BUTTON_B)) {
-
+    Serial.println("b");
+    delay(70);
   }
 }
 
 void buttonC () {
   if (buttonIsON(BUTTON_C)) {
-
+    Serial.println("c");
+    delay(30);
   }
 }
 
 
 bool buttonIsON (uint8_t button) {
   //delay(50);
-  if (digitalRead(button)==LOW && !buttonFlag) {
+  if (digitalRead(button)==LOW && !buttonFlag && millis()%50==0) {
     buttonFlag = true;
     sound(button);
     return 1;
   }
-  else if (digitalRead(button)==LOW && buttonFlag) {
+  else if (digitalRead(button)==LOW && buttonFlag && millis()%50==0) {
     return 0;
   }
   else {
---
+++ others.ino
@@ -0,0 +1,3 @@
+bool wait (int n) {
+  
+}
